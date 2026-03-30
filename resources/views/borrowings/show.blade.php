df4d448e17f8c686f7701c8a8abd2b85b225fa21
Moh Hefni <mohhefni2000@gmail.com>
2026-03-30 22:12:38 +0700

    fix: perbaikan detail peminjaman

--- resources/views/borrowings/show.blade.php
+++ resources/views/borrowings/show.blade.php
@@ -6,28 +6,54 @@
         <div class="section-header">
             <h1>Detail Peminjaman #{{ $borrowing->id }}</h1>
         </div>
-        <div class="card">
+        <div class="card card-primary">
             <div class="card-body">
-                <h6>Nama Peminjam: {{ $borrowing->member->name }}</h6>
-                <h6>Status: {{ ucfirst($borrowing->status) }}</h6>
-                <hr>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Judul Buku</th>
-                            <th>Qty</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($borrowing->details as $detail)
-                        <tr>
-                            <td>{{ $detail->book->title }}</td>
-                            <td>{{ $detail->qty }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <a href="{{ route('borrowings.index') }}" class="btn btn-danger">Kembali</a>
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <h6>Nama Peminjam: <span class="text-primary">{{ $borrowing->member->name }}</span></h6>
+                        <h6>Status: 
+                            @if($borrowing->status == 'borrowed')
+                                <span class="badge badge-warning">Sedang Dipinjam</span>
+                            @else
+                                <span class="badge badge-success">Sudah Dikembalikan</span>
+                            @endif
+                        </h6>
+                    </div>
+                    <div class="col-md-6 text-md-right">
+                        <small>Tgl Pinjam: <b>{{ $borrowing->borrow_date }}</b></small><br>
+                        <small class="text-danger">Batas Kembali: <b>{{ $borrowing->due_date }}</b></small>
+                        @if($borrowing->return_date)
+                            <br><small class="text-success">Tgl Kembali: <b>{{ $borrowing->return_date }}</b></small>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-md">
+                        <thead>
+                            <tr class="bg-light">
+                                <th width="10%">No</th>
+                                <th>Judul Buku</th>
+                                <th width="20%">Jumlah (Qty)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($borrowing->details as $index => $detail)
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
+                                <td>{{ $detail->book->title }}</td>
+                                <td>{{ $detail->qty }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="text-right mt-4">
+                    <a href="{{ route('borrowings.index') }}" class="btn btn-danger">
+                        <i class="fas fa-arrow-left"></i> Kembali
+                    </a>
+                </div>
             </div>
         </div>
     </section>
